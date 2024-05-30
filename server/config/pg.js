2406f5b66fd6aed4f6d6222b45dfdbdd3c571fa7
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-05-30 21:47:17 +0700

    restore index.js

--- server/config/pg.js
+++ server/config/pg.js
@@ -13,18 +13,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-const { Pool } = require("pg");
-require('dotenv').config(); 
-
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
-
-const pool = new Pool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  database: DB_NAME,
-  port: DB_PORT,
-  ssl: require
-});
-
-module.exports = pool;
