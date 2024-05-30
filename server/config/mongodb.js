c5e320bc72b7890799030c073d713653a91e2851
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-05-30 21:39:07 +0700

    resolve conflicts

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -14,19 +14,3 @@ const connectDatabase = async () => {
 };
 
 module.exports = connectDatabase;
-const mongoose = require("mongoose");
-
-const connectDatabase = async () => {
-  const URI = process.env.MONGODB_URI;
-
-  mongoose
-    .connect(URI)
-    .then(() => {
-      console.log("Connected to MongoDB.");
-    })
-    .catch((err) => {
-      console.log("Message: " + err.message);
-    });
-};
-
-module.exports = connectDatabase;
